54128ac6fb881c72eb0e98ee0be727843d253f6f
Luca D'Angelo <info@lucadangelo.it>
2018-11-16 07:15:56 +0000

    first

---
+++ Trees and Graphs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trees_and_Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
