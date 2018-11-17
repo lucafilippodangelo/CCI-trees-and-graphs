307fb7762996002f5196effc3ff9525ab9052454
Luca D'Angelo <info@lucadangelo.it>
2018-11-17 16:13:54 +0000

    4.1

---
+++ Q4_01_RouteBetweenNodes/Implementation.cs
@@ -0,0 +1,38 @@
+﻿using HelpLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q4_01_RouteBetweenNodes
+{
+    class Implementation
+    {
+        /// <summary>
+        /// Implementation of BFS, the method returns "-1" if no connection between start and end node, or shortest lenght of connecting path.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="start"></param>
+        /// <param name="End"></param>
+        /// <returns></returns>
+        public int search(Graph graph, Node startNode, Node EndNode)
+        {
+            /*
+             enqueue root node and mark as visited
+               while queue is not empty
+                 dequeue node + visit 
+                 foreach adjacent 
+                   mark if not already marked + enqueue             
+             */
+
+            LinkedList<Node> aNodeList = new LinkedList<Node>();
+            aNodeList.AddFirst(startNode);
+
+
+
+
+            return -1;//if not found
+        }
+
+
+    }
+}
---
+++ Q4_01_RouteBetweenNodes/Program.cs
@@ -0,0 +1,36 @@
+﻿using HelpLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace Q4_01_RouteBetweenNodes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LD node creation
+            
+            Node Node1 = new Node(1);
+            Node Node2 = new Node(2);
+            Node Node3 = new Node(3);
+            Node Node4 = new Node(4);
+            Node Node5 = new Node(5);
+            Node Node6 = new Node(6);
+            Node Node7 = new Node(7);
+
+            //LD Graph creation
+            Graph Graph1 = new Graph();
+            Graph1.Nodes = new List<Node> {Node1,Node2,Node3,Node4,Node5,Node6,Node7};
+
+            //LD Graph setup
+            // N7 visitable from N1, not from N2
+            // We can visit N4, N1->N3->N6->N4 or N1->N2->N4. The shortest path has two steps
+            Graph1.nodePointTo(Node1, new List<Node>() {Node2 ,Node3});
+            Graph1.nodePointTo(Node2, new List<Node>() {Node5, Node4});
+            Graph1.nodePointTo(Node3, new List<Node>() {Node6});
+            Graph1.nodePointTo(Node5, new List<Node>() {Node4});
+            Graph1.nodePointTo(Node6, new List<Node>() {Node4,Node7});
+
+        }//end main
+    }
+}
