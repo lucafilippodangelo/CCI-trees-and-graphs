b23885e73ffa43cb9bfed7c5b63e0459d72e459b
Luca D'Angelo <info@lucadangelo.it>
2018-11-29 08:09:34 +0000

    4.5

---
+++ Q4_05_ValidateBST/Implementation.cs
@@ -0,0 +1,54 @@
+﻿using HelpLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q4_05_ValidateBST
+{
+    class Implementation
+    {
+        static int maxFromLeft;
+        static int maxFromRight;
+        static int currentFromLeft;
+        static int currentFromRight;
+
+        //LD this is a complex approach I got, not sure it is working
+        /*
+         
+        public static int checkBSTRecursive2(Node aNode)
+        {
+            //LD mark the node as visited
+            aNode.visited = true;
+
+            if (aNode.leftPointingNode != null && aNode.leftPointingNode.visited == false)
+                maxFromLeft = checkBSTRecursive(aNode.leftPointingNode);
+
+            if (aNode.rightPointingNode != null && aNode.rightPointingNode.visited == false)
+                maxFromRight = checkBSTRecursive(aNode.rightPointingNode);
+
+            //LD CHECK if "left<=current<right" in case 
+
+            currentFromLeft = -1;
+            currentFromRight = -1;
+            if (aNode.leftPointingNode != null && !(aNode.LDvalue >= aNode.leftPointingNode.LDvalue) && !(aNode.LDvalue >= maxFromLeft)) 
+            {
+                //exit from function, the tree is not balanced  
+            }
+            else { currentFromLeft = aNode.leftPointingNode.LDvalue; }
+
+            if  (aNode.rightPointingNode != null && !(aNode.LDvalue < aNode.leftPointingNode.LDvalue) && !(aNode.LDvalue < maxFromRight))
+            {
+                //exit from function, the tree is not balanced
+            }
+            else { currentFromRight = aNode.rightPointingNode.LDvalue; }
+
+
+            return Math.Max(currentFromLeft, Math.Max(currentFromRight,aNode.LDvalue )); // at each call return: max between current, left and right
+        }
+
+        */
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -55,4 +55,10 @@ APPROACH:
 
 [**implemented in "//LD Q4_04_CheckBalanced"**](https://github.com/lucafilippodangelo/CCI-trees-and-graphs/tree/master/Q4_04_CheckBalanced)
 
-## 4.4 Validate BST
+## 4.5 Validate BST
+Check if the input tree is a BST, Binary Search Tree
+
+APPROACH:
+
+- OPTION 1 -> do a In-Order DFS, copy all the content of the tree in an array and then check if the array is ordered. An improvement could be to don't use the array, keep in a static variable the last visited node in order to compare it with the current.
+- OPTION 2 -> I went for a DFS implementation. An in-order search respecting in all the tree was "left <= current < right"
