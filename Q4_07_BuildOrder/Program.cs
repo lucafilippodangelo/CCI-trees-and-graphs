77a40c30a791d084aa1cd75044337a1d1041b77b
Luca D'Angelo <info@lucadangelo.it>
2018-12-08 19:50:05 +0000

    4.7

---
+++ Q4_07_BuildOrder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q4_07_BuildOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
