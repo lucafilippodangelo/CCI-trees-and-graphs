2eba304849abc812816c67d54b4b212728121614
Luca D'Angelo <info@lucadangelo.it>
2018-12-08 17:20:44 +0000

    successor beginning

---
+++ Q4_06_Successor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Q4_06_Successor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
